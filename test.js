1e98017968abf806803d4fc9dbde569b288ea423
Tom Randle <tom.randle@gmail.com>
2014-01-13 23:13:31 +0000

    initial commit

---
+++ test.js
@@ -0,0 +1,29 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
+
+
+
+var five = require("johnny-five"),
+    // or "./lib/johnny-five" when running from the source
+    board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create an Led on pin 13 and strobe it on/off
+  // Optionally set the speed; defaults to 100ms
+  (new five.Led(13)).strobe();
+  console.log('howdee hi');
+
+});
