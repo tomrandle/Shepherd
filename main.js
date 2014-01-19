f4dc9a34fd69aefbccc1984555bddc5cc78fa115
Tom Randle <tom.randle@gmail.com>
2014-01-19 13:18:31 +0000

    Fixed logging
    Wasn't creating a new object - so all values were updated.
    
    Prettified.

--- main.js
+++ main.js
@@ -1,206 +1,204 @@
+/********************/
+/* Global Variables */
+/********************/
+
 /* Constants */
 
 var TIME_INTERVAL = 400;
 
-var currentGameTime = 0;
+/* Global variables */
 
+var currentGameTime = 0;
 var keyPresses  = [];
 
-/* Johnny Five */
-
-var five = require("johnny-five");
-var board = new five.Board();
-
-/* Pins */
-
 var sensors = [
-	{
-		"position" : "front",
-		"pin" : 'A0',
-		"fivePin" : '',
-		"lastReading" : '',
-		"readings" :[]
-	},
-	{
-		"position" : "top",
-		"pin" : 'A1',
-		"fivePin" : '',
-		"lastReading" : '',
-		"readings" :[]
-	},
-	{
-		"position" : "middle",
-		"pin" : 'A2',
-		"fivePin" : '',
-		"lastReading" : '',
-		"readings" :[]
-	},
-	{
-		"position" : "bottom",
-		"pin" : 'A3',
-		"fivePin" : '',
-		"lastReading" : '',
-		"readings" :[]
-	}
-]
+    {
+        "position" : "front",
+        "pin" : 'A0',
+        "fivePin" : '',
+        "lastReading" : '',
+        "readings" :[]
+    },
+    {
+        "position" : "top",
+        "pin" : 'A1',
+        "fivePin" : '',
+        "lastReading" : '',
+        "readings" :[]
+    },
+    {
+        "position" : "middle",
+        "pin" : 'A2',
+        "fivePin" : '',
+        "lastReading" : '',
+        "readings" :[]
+    },
+    {
+        "position" : "bottom",
+        "pin" : 'A3',
+        "fivePin" : '',
+        "lastReading" : '',
+        "readings" :[]
+    }
+];
 
 var solenoids = [
     {
-		"position" : "top",
-		"pin" : 8,
-		"fivePin" : '',
-		"key" : "i",
-		"on" : false,
-		"unactionedKeypress" : false
-	},
-	{
-		"position" : "middle",
-		"pin" : 9,
-		"fivePin" : '',
-		"key" : "o",
-		"on" : false,
-		"unactionedKeypress" : false
-	},
+        "position" : "top",
+        "pin" : 8,
+        "fivePin" : '',
+        "key" : "i",
+        "on" : false,
+        "unactionedKeypress" : false
+    },
     {
-		"position" : "bottom",
-		"fivePin" : '',
-		"pin" : 10,
-		"key" : "p",
-		"on" : false,
-		"unactionedKeypress" : false
-	}];
-
-
-/* Sheep */
+        "position" : "middle",
+        "pin" : 9,
+        "fivePin" : '',
+        "key" : "o",
+        "on" : false,
+        "unactionedKeypress" : false
+    },
+    {
+        "position" : "bottom",
+        "fivePin" : '',
+        "pin" : 10,
+        "key" : "p",
+        "on" : false,
+        "unactionedKeypress" : false
+    }
+];
 
 var sheepList = [];
 
-var sheep = { 
-        "timeSpotted" : "",
-        "lane" :"",
-        "speed" : "",
-        "expectedArrivalTime" : "",
-    };
-
-/* Log */
+var sheep = {
+    "timeSpotted" : "",
+    "lane" :"",
+    "speed" : "",
+    "expectedArrivalTime" : ""
+};
 
 var logHistory = [];
 
-var logItem = {
-	'time' : 0,
-	'frontLDR' : '',
-	'topLDR' : '',
-	'middleLDR' :'',
-	'bottomLDR' :'',
-	'topSolenoid' : '',
-	'middleSolenoid' :'',
-	'bottomSolenoid' : ''
-};
-
-/* Variables */
-
-var readings = [];
-var newReadings = [];
-
+function logItem(time, frontLDR, topLDR, middleLDR, bottomLDR, topSolenoid, middleSolenoid, bottomSolenoid) {
+    this.time = time;
+    this.frontLDR = frontLDR;
+    this.topLDR = topLDR;
+    this.middleLDR = middleLDR;
+    this.bottomLDR = bottomLDR;
+    this.topSolenoid = topSolenoid;
+    this.middleSolenoid = middleSolenoid;
+    this.bottomSolenoid = bottomSolenoid;
+}
 
 
 /* Initiate Pins */
 
 function initiatePins() {
 
-	for (var i=0; i < sensors.length; i++) {
-		sensors[i].fivePin = new five.Pin(sensors[i].pin);
-	};
+    for (var i=0; i < sensors.length; i++) {
+        sensors[i].fivePin = new five.Pin(sensors[i].pin);
+    }
 
-	for (var i=0; i < solenoids.length; i++) {
-		solenoids[i].fivePin = new five.Pin(solenoids[i].pin);
-	};
-};
+    for (var i=0; i < solenoids.length; i++) {
+        solenoids[i].fivePin = new five.Pin(solenoids[i].pin);
+    }
+}
 
-
-/* Read Pin */ 
+/* Read Pin */
 
 function readPin(pin) {
-	var reading = pin.value;
-	return reading;
-};
+    var reading = pin.value;
+    return reading;
+}
 
 function updateAllReadings() {
 
-	for (var i=0; i < sensors.length; i++) {
+    for (var i=0; i < sensors.length; i++) {
 
-		var reading = readPin(sensors[i].fivePin);
+        var reading = readPin(sensors[i].fivePin);
 
-		sensors[i].lastReading = reading;
-		sensors[i].readings.push(reading);
+        sensors[i].lastReading = reading;
+        sensors[i].readings.push(reading);
 
-	};
-};
+    }
+}
 
-/* Check keyboard input and */
+/* Check keyboard input */
 
 function queueKeyPresses() {
     
-	for (var i = 0; i < solenoids.length; i++) {
+    for (var i = 0; i < solenoids.length; i++) {
 
-		var x = i;
-		console.log(keyPresses);
+        var x = i;
+        console.log(keyPresses);
 
-		for(var j = keyPresses.length -1; j >= 0 ; j--) {
-			if (keyPresses[j] == solenoids[x].key)
-			{
-				keyPresses.splice(j, 1);  
-				solenoids[x].unactionedKeypress = true;
-			}
-		};
-	}
+        for(var j = keyPresses.length -1; j >= 0 ; j--) {
+            if (keyPresses[j] == solenoids[x].key)
+            {
+                keyPresses.splice(j, 1);
+                solenoids[x].unactionedKeypress = true;
+            }
+        }
+    }
 
-};
-
-
-
-/* Fire solenoid */ 
-
-function fireSolenoid(pin) {
-	pin.high();
 }
 
-function turnOffSolenoid(pin) {
-	pin.low();
+/* Solenoid firing*/
+
+function fireSolenoid(solenoid) {
+    solenoids[solenoid].fivePin.high();
+    solenoids[solenoid].on  = true;
 }
 
+function turnOffSolenoid(solenoid) {
+    solenoids[solenoid].fivePin.high();
+    solenoids[solenoid].on  = true;
+}
 
 function checkSolenoids() {
-	for (var i = 0; i < solenoids.length; i++) {
 
-		if (solenoids[i].unactionedKeypress === true) {
-			fireSolenoid(solenoids[i].fivePin);
-		};
-	};
+    for (var i = 0; i < solenoids.length; i++) {
+
+    /* Read current state */
+
+        if (solenoids[i].unactionedKeypress === true) {
+            fireSolenoid(i);
+        }
+    }
 }
 
 
 /* Upadte log file */
 
 function updateLog() {
-	logItem.time = currentGameTime;
-	logItem.frontLDR = sensors[0].lastReading;
-	logItem.topLDR = sensors[1].lastReading;
-	logItem.middleLDR = sensors[2].lastReading;
-	logItem.bottomLDR = sensors[3].lastReading;
-	logItem.topSolenoid = solenoids[0].on;
-	logItem.middleSolenoid = solenoids[1].on;
-	logItem.bottomSolenoid = solenoids[2].on;
 
-	logHistory.push(logItem);
+    var latestLog = new logItem (
+        currentGameTime,
+        sensors[0].lastReading,
+        sensors[1].lastReading,
+        sensors[2].lastReading,
+        sensors[3].lastReading,
+        solenoids[0].on,
+        solenoids[1].on,
+        solenoids[2].on
+    )
+    
 
-	var fs = require('fs');
+    logHistory.push(latestLog);
+
 }
 
+var fs = require('fs');
 
 function writeReadingsToFile() {
-	fs.writeFile("log.txt", JSON.stringify(logHistory)); 
+
+
+    console.log(logHistory);
+
+    fs.writeFile("log.txt", JSON.stringify(logHistory));
 }
+
 
 
 /********************/
@@ -208,61 +206,60 @@ function writeReadingsToFile() {
 /********************/
 
 
-/* Keyboard listening */
-
+/* Set up keyboard listening */
 
 var stdin = process.stdin;
 
 stdin.setRawMode( true );
-
 stdin.resume();
-
 stdin.setEncoding( 'utf8' );
 
+/* Listen for input */
 
-// Listen for input 
+stdin.on( 'data', function( key ) {
 
-stdin.on( 'data', function( key ){
-  
   // Exit if ctrl-c
-
-  if ( key === '\u0003' ) {
-    process.exit();
-  }
-
-  keyPresses.push(key);
-
+    if ( key === '\u0003' ) {
+        process.exit();
+    }
+    keyPresses.push(key);
 });
 
 
-/* Set up board */
+
+/* Set up Johnny Five */
+
+var five = require("johnny-five");
+var board = new five.Board();
 
 board.on("ready", function() {
 
-	('Applicaton started');
+    ('Applicaton started');
 
-	initiatePins();
+    initiatePins();
+
+
 
 /* Main loop */
 
 setInterval(function(){
-	
-	updateAllReadings();
-	writeReadingsToFile();
+    
+    updateAllReadings();
+    writeReadingsToFile();
 
-	checkSolenoids();
-	// console.log(readings);
+    checkSolenoids();
+    // console.log(readings);
 
 
-	console.log(keyPresses);
-	queueKeyPresses();
+    console.log(keyPresses);
+    queueKeyPresses();
 
-	updateLog();
+    updateLog();
 
-	currentGameTime += TIME_INTERVAL;
+    currentGameTime += TIME_INTERVAL;
 
-	},TIME_INTERVAL);
-	
+    },TIME_INTERVAL);
+    
   });
 
 
