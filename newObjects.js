949efec08102d725f5d49d77a219656ff1efc444
Tom Randle <tom.randle@gmail.com>
2014-01-24 22:40:04 +0000

    updated time function

--- newObjects.js
+++ newObjects.js
@@ -5,7 +5,6 @@ var five = require("johnny-five");
 var Sensor = require('./sensor.js');
 var Solenoid = require('./solenoid.js');
 
-
 /* Constants */ 
 
 var GAME_INTERVAL = 100;
@@ -13,35 +12,15 @@ var SOLENOID_PRESS_TIME = 300;
 
 /* Global Variables */ 
 
-var currentTime = 0;
 var keyPresses = [];
-
 
 /*Objects*/
 
 function game() {
 
-    // Keyboard input
+    // Check keyboard
 
-    var stdin = process.stdin;
-
-
-    stdin.setRawMode( true );
-    stdin.resume();
-    stdin.setEncoding( 'utf8' );
-
-    stdin.on( 'data', function( key ) {
-
-      // Exit if ctrl-c
-        if ( key === '\u0003' ) {
-            process.exit();
-        }
-
-        lastKeyPress = new keyPress(key, currentTime);
-        keyPresses.push(lastKeyPress);
-
-    });
-
+    keyboard();
 
     // Connect to board
 
@@ -69,31 +48,18 @@ function game() {
 
             topLane.decideWhetherToFireSolenoid();
             
-            // Update time 
-            console.log(currentTime);
-            currentTime = currentTime + GAME_INTERVAL;
 
          }, GAME_INTERVAL);
 
     });
 }
 
-
 game();
-
-
 
 function Lane(sensorPin, sensorThreshold, sensorDelay, solenoidPin, solenoidKey) {
     
-    // this.sensorPin = sensorPin;
-    // this.sensorThreshold = sensorThreshold;
-    // this.sensorDelay = sensorDelay;
-    // this.solenoidPin = solenoidPin;
-    // this.solenoidKey = solenoidKey;
-
     this.sensor = new Sensor (sensorPin, sensorThreshold);
     this.solenoid = new Solenoid (solenoidPin);
-
     this.alreadyActive = false; 
     this.sheepQueue = [];
 
@@ -118,41 +84,60 @@ function Lane(sensorPin, sensorThreshold, sensorDelay, solenoidPin, solenoidKey)
 
         // Check if keyboard pressed 
 
+        this.solenoid.turnOffSolenoid();
+
+
         for (var i=0; i < keyPresses.length; i++) {
+
 
             var timeKeyPressed = keyPresses[i].timePressed;
 
-            if (keyPresses[i].key === solenoidKey && currentTime < timeKeyPressed + SOLENOID_PRESS_TIME) {
+            if (keyPresses[i].key === solenoidKey && currentTime() < timeKeyPressed + SOLENOID_PRESS_TIME) {
+
+                console.log(timeKeyPressed, currentTime())
+
                 this.solenoid.fireSolenoid();
             }
 
-            else {
-                this.solenoid.turnOffSolenoid();
-            }
         }
         
         // Check if sheep arrived
-
-
     }
-
-
-
 }
 
 
+// Functions
 
+function keyboard() {
+    var stdin = process.stdin;
+    stdin.setRawMode( true );
+    stdin.resume();
+    stdin.setEncoding( 'utf8' );
 
+    stdin.on( 'data', function( key ) {
 
+      // Exit if ctrl-c
+        if ( key === '\u0003' ) {
+            process.exit();
+        }
 
+        lastKeyPress = new keyPress(key, currentTime());
+        keyPresses.push(lastKeyPress);
+
+    });
+}
+
+function currentTime() {
+    var date = new Date();
+    return date.getTime();
+}
+
+// Objects
 
 function Sheep (timeSpotted) {
     this.timeSpotted = timeSpotted;
     this.activeSheep = false;
 }
-
-
-
 
 function keyPress(key, timePressed) {
     this.key = key;
