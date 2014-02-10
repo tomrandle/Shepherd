8ee024186b7a1218c55364cf6f458d53c258d808
Tom Randle <tom.randle@gmail.com>
2014-02-10 22:40:55 +0000

    Added second solenoids

--- main.js
+++ main.js
@@ -58,15 +58,15 @@ function game() {
         var lanes = [
             {
                 'name' : 'topLane',
-                'lane' : new Lane('A2',1.02,400,5,'i',8),
+                'lane' : new Lane('A2',1.02,400,5,'i',8,2,500),
             },
             {
                 'name' : 'middleLane',
-                'lane' : new Lane('A1',1.03,250,6,'o',9),
+                'lane' : new Lane('A1',1.03,250,6,'o',9,3,500),
             },
             {
                 'name' : 'bottomLane',
-                'lane' : new Lane('A0',1.03,400,7,'p',10),
+                'lane' : new Lane('A0',1.03,400,7,'p',10,4,500),
             }
         ];
 
@@ -133,7 +133,7 @@ function game() {
 
 game();
 
-function Lane(sensorPin, sensorThreshold, sensorDelay, solenoidPin, solenoidKey, ledPin) {
+function Lane(sensorPin, sensorThreshold, sensorDelay, solenoidPin, solenoidKey, ledPin, secondSolenoidPin, secondDelay) {
 
     this.sensor = new Sensor (sensorPin, sensorThreshold);
     this.solenoid = new Solenoid (solenoidPin);
@@ -143,6 +143,8 @@ function Lane(sensorPin, sensorThreshold, sensorDelay, solenoidPin, solenoidKey,
     this.solenoidKey = solenoidKey;
     this.sensorDelay = sensorDelay;
     this.solenoidPin = solenoidPin;
+    this.secondSolenoid = new Solenoid (secondSolenoidPin);
+    this.secondDelay = secondDelay;
 
     this.activeSensor = function () {
         return this.sensor.isTriggered();
@@ -167,9 +169,12 @@ function Lane(sensorPin, sensorThreshold, sensorDelay, solenoidPin, solenoidKey,
 
     this.decideWhetherToFireSolenoid = function () {
 
-        // Check if keyboard pressed 
+        // Set solenoids to off
 
         this.solenoid.turnOffSolenoid();
+        this.secondSolenoid.turnOffSolenoid();
+
+        // Check if keyboard pressed 
 
         for (var i=0; i < keyPresses.length; i++) {
 
@@ -187,8 +192,14 @@ function Lane(sensorPin, sensorThreshold, sensorDelay, solenoidPin, solenoidKey,
 
             var timeSpotted = this.sheepQueue[i].timeSpotted;
 
+            // Todo: tidy up these calculations
+            
             if (currentTime() < timeSpotted + this.sensorDelay + SOLENOID_PRESS_TIME && currentTime() > timeSpotted + this.sensorDelay) {
                 this.solenoid.fireSolenoid();
+            }
+
+            if (currentTime() < timeSpotted + this.sensorDelay + this.secondDelay + SOLENOID_PRESS_TIME && currentTime() > timeSpotted + this.sensorDelay + this.secondDelay) {
+                this.secondSolenoid.fireSolenoid();
             }
         }
 
