61a4aea88172065515cd063469c5c0ad003d3a25
Tom Randle <tom.randle@gmail.com>
2014-02-15 17:57:36 +0000

    added px units to size

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -5,5 +5,5 @@ var WIDTH_TO_HEIGHT_RATIO = 16/9;
 
 $( window ).resize(function() {
     var frameWidth = $( ".wrapper" ).width();
-    $('figure iframe').height(frameWidth / WIDTH_TO_HEIGHT_RATIO);
+    $('figure iframe').height("'" + frameWidth / WIDTH_TO_HEIGHT_RATIO + "px'");
 });
