79f85c5b5ac7e0161b99e83300659f478afb6326
Tom Randle <tom.randle@gmail.com>
2014-02-15 18:10:02 +0000

    refactored sizing css

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -1,18 +1,22 @@
+var WIDTH_TO_HEIGHT_RATIO = 16/9;
 
 $( document ).ready(function() {
 
-    /* Set iframe heights in responsive layout */
-
-    var WIDTH_TO_HEIGHT_RATIO = 16/9;
+    sizeVideos();
 
     $( window ).resize(function() {
+       sizeVideos();
+    });
+
+    /* Set iframe heights in responsive layout */
+
+    function sizeVideos() {
         var frameWidth = $( ".wrapper" ).width();
         var frameHeight = frameWidth / WIDTH_TO_HEIGHT_RATIO;
         var cssHeight = frameHeight + 'px';
 
         $('figure iframe').height(cssHeight);
-
-    });
+    }
 
 });
 
