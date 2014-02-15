4ecd045f32c47929fb9e28e8b35180ff0513e09c
Tom Randle <tom.randle@gmail.com>
2014-02-15 17:49:01 +0000

    Added js to set iframe sizes correctly on mobile.

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -1,1 +1,9 @@
 
+/* Set iframe heights in responsive layout */
+
+var WIDTH_TO_HEIGHT_RATIO = 16/9;
+
+$( window ).resize(function() {
+    var frameWidth = $( ".wrapper" ).width();
+    $('figure iframe').height(frameWidth / WIDTH_TO_HEIGHT_RATIO);
+});
